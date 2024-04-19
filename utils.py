d2aea281580a7e8282fa7a8750c50cd84c8d67a2
nem0z <maxime.bernard.dev@gmail.com>
2024-04-19 15:50:40 +0200

    feat: switch to hashlib

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
-from Cryptodome.Hash import SHA256, RIPEMD160
+import hashlib
 
 def checksum(payload: bytes) -> bytes:
-    return SHA256.new(SHA256.new(payload).digest()).digest()[:4]
+    return hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
 
 
 def hash160(payload: bytes) -> bytes:
-        sha256hash = SHA256.new(payload).digest()
-        return RIPEMD160.new(sha256hash).digest()
+        sha256hash = hashlib.sha256(payload).digest()
+        return hashlib.new('ripemd160', sha256hash).digest()
